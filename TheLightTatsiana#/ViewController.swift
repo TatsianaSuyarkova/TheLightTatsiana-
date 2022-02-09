05dfed5aa7d3245c27cdfdbcb56b325b4847c9a1
Tatsiana Suyarkova <tankor@bk.ru>
2022-02-09 14:57:27 +0300

    Initial Commit

---
+++ TheLightTatsiana#/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheLightTatsiana#
+//
+//  Created by Дмитрий on 09.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
